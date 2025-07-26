3a49639290d374cf6e1af22ba586a101d83ec4c4
Rohit Shukla <shuklarohit2105@gmail.com>
2025-07-26 23:03:13 +0530

    feat(user): create User model with schema definition

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['customer', 'admin']
+  }
+}, {
+  timestamps: true
+});
+
+export const User = mongoose.model('User', userSchema);
